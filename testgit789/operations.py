9c3719795c654803d48bee84e2be1db56ac6ffb5
aymeric boffa <aymeric.boffa@gmail.com>
2022-02-28 09:59:16 +0100

    created custom_operation

--- testgit789/operations.py
+++ testgit789/operations.py
@@ -7,3 +7,9 @@ def addition(a, b):
     Implementation of addition operation
     """
     return a+b
+
+def custom_operation(a,b,c):
+    """
+    Implementation of a custom operation
+    """
+    pass
